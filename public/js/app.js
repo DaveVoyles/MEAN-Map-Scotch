2cf60a6a335254208ed1cd42e6d67310b6da1814
Dave Voyles <Startingline13@hotmail.com>
2016-03-11 15:36:35 -0500

    Added controller for join and find

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
-var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+
+    // Configures Angular routing -- showing the relevant view and controller when needed.
+    .config(function($routeProvider){
+
+        $routeProvider
+           // Join Team Control Panel
+          .when('/join', {
+            controller:  'addCtrl', 
+            templateUrl: 'partials/addForm.html',
+
+          // Find Teammates Control Panel
+        }).when('/find', {
+            templateUrl: 'partials/queryForm.html',
+
+            // All else forward to the Join Team Control Panel
+        }).otherwise({redirectTo:'/join'})
+    });
